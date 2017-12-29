7f0d21df9007d78fa6f33155f62226fcbc91c4f3
Xin <lovemarks19@gmail.com>
2017-12-29 22:19:35 +0800

    修改 RegisterController 利用多型關聯將會員註冊資料寫入資料庫

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Member;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
@@ -51,6 +52,7 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'gender' => 'required',
         ]);
     }
 
@@ -62,10 +64,23 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
+        Member::create([
+            'gender'=>$data['gender'],
         ]);
+
+        $member=Member::orderByDesc('created_at')->first();
+
+        /*$user = User::create([
+            'name'=>$data['name'],
+        'email'=>$data['email'],
+        'password' => bcrypt($data['password']),
+        ]);*/
+        $user=new User();
+        $user->name=$data['name'];
+        $user->email=$data['email'];
+        $user->password = bcrypt($data['password']);
+
+        $member->user()->save($user);
+        return $user;
     }
 }
