a94daf88a1064410c1a8c8199e49b69a83a07e91
陳泓維 <tim5556618@gmail.com>
2018-01-07 21:52:17 +0800

    路由修正，需登入否則轉跳首頁

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Auth;
+
 class CommentsController extends Controller
 {
-    //
+    public function index()
+    {
+
+        return view('comments.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,19 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/stores', 'StoresController@index')->name('stores.index');
 
+Route::group(['middleware'=>'auth'],function(){
 
-Route::get('/comments', 'CommentsController@index')->name('comments.index');
-//瀏覽自己評價
-Route::get('/comments/{comment_id}', 'CommentsController@destroy')->name('comments.destroy');
-//刪評價
-Route::get('/comments/{comment_id}/edit', 'CommentsController@edit')->name('comments.edit');
-Route::patch('/comments/{comment_id}', 'CommentsController@update')->name('comments.update');
-//修改評價
-Route::get('/comments/create', 'CommentsController@create')->name('comments.create');
-Route::post('/comments', 'CommentsController@store')->name('comments.store');
-//新增評價
+    Route::get('/comments', 'CommentsController@index')->name('comments.index');
+    //瀏覽自己評價
+
+    Route::get('/comments/{comment_id}', 'CommentsController@destroy')->name('comments.destroy');
+    //刪評價
+
+    Route::get('/comments/{comment_id}/edit', 'CommentsController@edit')->name('comments.edit');
+    Route::patch('/comments/{comment_id}', 'CommentsController@update')->name('comments.update');
+    //修改評價
+
+    Route::get('/comments/create', 'CommentsController@create')->name('comments.create');
+    Route::post('/comments', 'CommentsController@store')->name('comments.store');
+    //新增評價
+});
